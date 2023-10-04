001e4b2e97862b2c2ecab835d9fb8bb6eab90975
cc799033 <caitlin4c@gmail.com>
2023-10-04 18:38:20 -0400

    Updated if statements

--- app/src/main/java/com/zybooks/tictactoe/MainActivity.java
+++ app/src/main/java/com/zybooks/tictactoe/MainActivity.java
@@ -6,17 +6,17 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //1 = O's, 2=X's
     int player = 1;
     int GameState[] = {0,0,0,0,0,0,0,0,0};
-    int[][] Winning = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{6,4,2}};
-
+    int [][] Winning = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    Boolean GameOn = true;
     //Shows whose turn it is
     TextView turn;
+    int plays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,43 +24,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     //This is called when one of the spots is clicked
     public void tapped(View view){
         ImageView img = (ImageView) view;
-
-        //Get tag to string so that we can use it for arrays
         String tag = img.getTag().toString();
+        plays++;
 
-        GameState[Integer.parseInt(tag)]=player;
+        if(GameOn && GameState[Integer.parseInt(tag)] == 0) {
+            //Get tag to string so that we can use it for arrays
 
-        // Message to change depending on whose turn it is
-        turn = (TextView) findViewById(R.id.turn);
+            GameState[Integer.parseInt(tag)] = player;
+
+            // Message to change depending on whose turn it is
+            turn = (TextView) findViewById(R.id.turn);
 
 
-        if(player == 1){
-            img.setImageResource(R.drawable.o);
-            player=2;
-            turn.setText("X's Turn");
-        }
-        else{
-            img.setImageResource(R.drawable.x);
-            player=1;
-            turn.setText("O's Turn");
-        }
+            if (player == 1) {
+                img.setImageResource(R.drawable.o);
+                player = 2;
+                turn.setText("X's Turn");
+            } else {
+                img.setImageResource(R.drawable.x);
+                player = 1;
+                turn.setText("O's Turn");
+            }
 
-        for (int[] Winning : Winning){
-            if (GameState[Winning[0]] == GameState[Winning[1]] && GameState[Winning[1]] == GameState[2] && GameState[Winning[0]] != 0){
-                String winner = " ";
-                if(player == 1) {
-                    winner = "X";
-                    turn = null;
+            for (int[] Winnings : Winning) {
+                if(plays == 9) {
+                    turn.setText("CAT");
                 }
-                else {
-                    winner = "O";
-                    turn = null;
+                else if (GameState[Winnings[0]] == GameState[Winnings[1]] && GameState[Winnings[1]] == GameState[Winnings[2]] && GameState[Winnings[0]] != 0) {
+                    GameOn = false;
+                    String winner = " ";
+                    if (player == 1)
+                        winner = "X";
+                    else
+                        winner = "O";
+                    turn.setText(winner + " wins!");
                 }
-                Toast.makeText(this, winner + " wins!", Toast.LENGTH_LONG).show();
             }
         }
     }
 }
+
